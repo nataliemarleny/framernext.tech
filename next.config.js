cc1e82eb526d0bc1796d84c8ffb8d03fc130d8f5
Natalie Marleny <nataliemarleny@gmail.com>
2019-08-14 00:40:55 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-const withSass = require('@zeit/next-sass')
-const withReactSvg = require('next-react-svg')
-const path = require('path')
-module.exports = withReactSvg(withSass({
-  include: path.join(__dirname, 'svg'),
-  experimental: {
-    modern: true,
-    granularChunks: true
-  }
-}))
+const withSass = require("@zeit/next-sass");
+const withReactSvg = require("next-react-svg");
+const path = require("path");
+module.exports = withSass(
+  withReactSvg({
+    include: path.join(__dirname, "svg"),
+    experimental: {
+      modern: true,
+      granularChunks: true
+    }
+  })
+);
